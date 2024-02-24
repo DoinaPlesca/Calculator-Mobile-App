d52175c73d7e3b4dab685c54a34f115d26eb1925
Doina Plesca <doinitzaplesca@yahoo.com>
2024-02-24 12:58:32 +0100

    added basic structure

---
+++ lib/calculator_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+import 'calculator_screen_state.dart';
+
+class CalculatorScreen extends StatefulWidget {
+  const CalculatorScreen({Key? key}) : super(key: key);
+
+  @override
+  CalculatorScreenState createState() => CalculatorScreenState();
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'calculator_screen.dart';
+
+void main() => runApp(const CalculatorApp());
+
+class CalculatorApp extends StatelessWidget {
+  const CalculatorApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'RPN Calculator',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const CalculatorScreen(),
+    );
+  }
+}
